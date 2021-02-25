25203f16f12cab207a23dd09d3e1ea3a003c3538
victorSistem <62269097+victorSistem@users.noreply.github.com>
2021-02-25 18:05:01 -0400

    subiendo proyecto 1 landing page portafolio - cv

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 #Taller de maquetacion
+
+## Proyectos
+
+- [Landing Page de tipo Portafolio-CV](https://victorSistem.github.io/Maquetacion-Web.git/portafolio-cv)
---
+++ portafolio-cv/scripts.js
@@ -0,0 +1,78 @@
+/*Menu*/
+((d)=>{
+const $btnMenu = d.querySelector('.menu-btn'),
+$menu = d.querySelector('.menu');
+
+$btnMenu.addEventListener('click', (e)=>{
+$btnMenu.firstElementChild.classList.toggle('none');
+$btnMenu.lastElementChild.classList.toggle('none');
+$menu.classList.toggle('is-active');
+
+});
+
+d.addEventListener('click', (e) => {
+
+    if(!e.target.matches('.menu a')) return false;
+    $btnMenu.firstElementChild.classList.remove('none');
+    $btnMenu.lastElementChild.classList.add('none');
+    $menu.classList.remove('is-active');
+
+});
+
+
+
+
+})(document);
+
+
+
+/*ContactForm*/
+((d)=>{
+    const $form = d.querySelector('.contact-form'),
+    $loader = d.querySelector('.contact-form-loader'),
+    $response = d.querySelector('.contact-form-response');
+
+    $form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    $loader.classList.remove('none');
+    fetch("https://formsubmit.co/ajax/3505f12a346a5e3d7afbf5296b36ff8a ", {
+        method:"POST",
+        body: new FormData(e.target)
+
+    }
+
+    
+    ).then(res=> res.ok ? res.json() : Promise.reject(res)).then(json=> {
+        console.log(json);
+    
+        location.hash = "#gracias";
+        $form.reset();
+
+    }).catch(
+        err=>{
+            console.log(err);
+            let message = err.status.Text || "Ocurrio un error al enviar, intenta nuevamente";
+            $response.querySelector("h3").innerHTML= `Error ${message}`;
+   
+
+        }
+    ).finally(()=>{
+        $loader.classList.add("none")
+
+        setTimeout(() => {
+            location.hash = "#close";
+        }, 3000);
+
+    });
+
+
+    });
+    
+
+    
+    
+    
+    
+    })(document);
+    
+
