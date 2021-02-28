8d2b07c80e12160a5879a867e258d6c5e692b660
victorSistem <62269097+victorSistem@users.noreply.github.com>
2021-02-28 18:42:01 -0400

    pwa

--- portafolio-cv/sw.js
+++ portafolio-cv/sw.js
@@ -5,65 +5,56 @@ urlsToCache = ['./',
 'https://fonts.gstatic.com',
 'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css',
 './style.css',
-'./script.js',
+'./scripts.js',
 './img/ProgramadorFitness.png',
 './img/favicon.png'
 ];
 
-//durante la fase de instalacion, generalmente se almacena en cache los activos estaticos
-self.addEventListener('install', e=>{
-
+//durante la fase de instalación, generalmente se almacena en caché los activos estáticos
+self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE_NAME)
-    .then(cache =>{
-      return cache.addAll(urlsToCache)
-      .then(()=>{
-        self.skip.Waiting()
-
+      .then(cache => {
+        return cache.addAll(urlsToCache)
+          .then(() => self.skipWaiting())
       })
-    }).catch(err => console.log("Fallo registro de cache", err))
-  );
-
-});
-
-// una vez que se instala el sw, se activa y busca los recursos para hacer que funcione sin conexion
-self.addEventListener('activate' ,e=>{
-const cacheWhitelist = [CACHE_NAME]
-e.waitUntil(
-  caches.keys()
-  .then(cachesNames=>{
-
-    cachesNames.map(cachesName =>{
-      //eliminamos lo que ya no se necesite en cache
-
-      if (cacheWhitelist.indexOf(cachesName) === -1){
-
-        return cache.delete(cachesName)
-      }
-
-    })
-  })
-  //le indica el sw activar el cache actual
-  .then(()=> self.clients.claim())
-
-)
-
-});
-
-//cuando el navegador recupera una url
-self.addEventListener('fetch',  e=>{
-// responde ya sea con el objeto en cache  continuar y buscar la url real.
-caches.match(e.request)
-.then(res=>{
-if(res){
-//recuperar del cache
-return res
-
-}
-
-// recuperar de la peticion la url
-return fetch(e.request)
-
+      .catch(err => console.log('Falló registro de cache', err))
+  )
 })
 
-});
+//una vez que se instala el SW, se activa y busca los recursos para hacer que funcione sin conexión
+self.addEventListener('activate', e => {
+  const cacheWhitelist = [CACHE_NAME]
+
+  e.waitUntil(
+    caches.keys()
+      .then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            //Eliminamos lo que ya no se necesita en cache
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName)
+            }
+          })
+        )
+      })
+      // Le indica al SW activar el cache actual
+      .then(() => self.clients.claim())
+  )
+})
+
+//cuando el navegador recupera una url
+self.addEventListener('fetch', e => {
+  //Responder ya sea con el objeto en caché o continuar y buscar la url real
+  e.respondWith(
+    caches.match(e.request)
+      .then(res => {
+        if (res) {
+          //recuperar del cache
+          return res
+        }
+        //recuperar de la petición a la url
+        return fetch(e.request)
+      })
+  )
+})
