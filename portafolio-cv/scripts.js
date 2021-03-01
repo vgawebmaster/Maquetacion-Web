65c7d9bca423b71166485b4474b7e376de0ca580
victorSistem <62269097+victorSistem@users.noreply.github.com>
2021-02-28 20:13:56 -0400

    test

--- portafolio-cv/scripts.js
+++ portafolio-cv/scripts.js
@@ -73,10 +73,11 @@ d.addEventListener('click', (e) => {
     
     
 
+ 
+    })(document);
+    
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker.register('./sw.js')
           .then(reg => console.log('Registro de SW exitoso', reg))
           .catch(err => console.warn('Error al tratar de registrar el sw', err))
       }
-    })(document);
-    
